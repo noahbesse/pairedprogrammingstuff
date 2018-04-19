bddeca4132cfe81ae740f6b917f36ffe8eb984ae
Besse0100816 <0100816@sps-student.sk.ca>
2018-04-19 14:44:23 -0600

    added classes

--- brickBreaker/sketch.js
+++ brickBreaker/sketch.js
@@ -1,34 +1,33 @@
-// p5js template project - replace with project title
-// Dan Schellenberg - replace with your name
-// Feb 2, 2018 - replace with the date
+// Brickbreaker
+// Tony and Noah
+// April 19th, 2018
 
-// global variables
-let gear;
 
-// the preload function guarentees that the code inside the function is
-// executed before the rest of the program runs -- helpful for things
-// like loading images (since JS is asynchronous)
-function preload() {
-  gear = loadImage("images/gear.png");
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Ball{
+  constructor(){
+    this.x;
+    this.y;
+    this.speed;
+  }
 }
 
-// the setup function will only run once (before the draw loop begins)
-// this is where you want to set up the environment (size of canvas, etc)
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
-
-function draw() {
-  background(255);
-
-  image(gear, 0, 0);
-
-  stroke(0);
-  line(0, 0, 200, 200);
-
-  fill(0, 255, 0, 100);
-  noStroke();
-
-  rect(mouseX, mouseY, 100, 300);
-  ellipse(400, 150, 300, 200);
+class Platform{
+  constructor(){
+    this.x = mouseX;
+    this.y = 700;
+    this.length;
+  }
 }
