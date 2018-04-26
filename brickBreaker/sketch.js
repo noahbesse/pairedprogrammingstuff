f2e771f49d43b00b9c9f7613287f0742666695dc
noahbesse <36053916+noahbesse@users.noreply.github.com>
2018-04-26 15:02:18 -0600

    centered canvas

--- brickBreaker/sketch.js
+++ brickBreaker/sketch.js
@@ -6,9 +6,15 @@ let state;
 let introScreen, finalLevel;
 let platform, redBrick, blueBrick, yellowBrick, greenBrick, greyBrick;
 let finalLevelMusic;
+let levelOfDifficulty;
 
 
 function preload(){
+  redBrick = loadImage("assets/redBrick.png");
+  blueBrick = loadImage("assets/blueBrick.png");
+  yellowBrick = loadImage("assets/yelowBrick.png");
+  greenBrick = loadImage("assets/greenBrick.png");
+  greyBrick = loadImage("assets/greyBrick.png");
   introScreen = loadImage("assets/introScreen.jpg");
   finalLevel = loadImage("assets/final.jpg");
   finalLevelMusic = loadSound("assets/FINALBOSSPOWERUP.wav");
@@ -18,8 +24,13 @@ function preload(){
 
 
 function setup() {
-  createCanvas(800, 800);
+  noCursor();
+  let canvas =  createCanvas(800, 800);
+  canvas.position(400,0);
   state = 1;
+}
+function placement() {
+  image(platform, mouseX, 700);
 }
 
 function draw() {
@@ -37,10 +48,10 @@ function startScreen(){
     textSize(150);
     textStyle(BOLD);
     textFont("Agency FB");
-    text("B R E A K E R", width / 2, height / 2 - 150);
+    text("B R E A K E R", 400,250);
 
     textSize(32);
-    text("P r e s s   S P A C E   t o   p l a y", width / 2, height / 2 + 150);
+    text("P r e s s   S P A C E   t o   p l a y", 400, 550);
 
     if (keyIsPressed){
       if (keyCode === 32){
@@ -49,31 +60,14 @@ function startScreen(){
     }
   }
 }
+function brickSpawn(){
+
+}
 
 function gameScreens() {
   if (state === 2) {
-    background(255);
+    background(introScreen);
     placement();
-  }
-}
-
-function placement() {
-  image(platform, mouseX, 700);
-
-}
-
-class Ball{
-  constructor(){
-    this.x;
-    this.y;
-    this.speed;
-  }
-}
-
-class Platform{
-  constructor(){
-    this.x = mouseX;
-    this.y = 700;
-    this.length;
+    brickSpawn();
   }
 }
