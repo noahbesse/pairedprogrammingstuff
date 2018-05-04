d63213ae47a130c28e8bc84aedd42408b95d9ab7
Tony Li <tb543666@gmail.com>
2018-05-04 00:08:35 -0600

    final

--- brickBreaker/sketch.js
+++ brickBreaker/sketch.js
@@ -3,10 +3,10 @@
 // April 19th, 2018
 
 // Variables
-let state;
+let state = 1;
 let introScreen, finalLevel;
 let platform, redBrick, blueBrick, yellowBrick, greenBrick, greyBrick, greekBrick;
-let finalLevelMusic, gameMusic;
+let winScreenMusic, finalLevelMusic, gameMusic;
 let ballEllipse;
 let ballSpawned;
 let x = 250;
@@ -15,8 +15,8 @@ let limits;
 let ballSize;
 let dx = 5;
 let dy = -5;
-let brickRowCount = 1;
-let brickColumnCount = 1;
+let brickRowCount = 10;
+let brickColumnCount = 7;
 let brickX, brickY;
 let brickTopOffset = 20;
 let brickSideOffset = 18.5;
@@ -27,7 +27,7 @@ let bricks = [];
 let brickStatusState = true;
 let level;
 let bounce;
-let score;
+let score, visualScore;
 
 // Disables right-click on the mouse
 document.addEventListener("contextmenu", event => event.preventDefault());
@@ -46,21 +46,25 @@ function preload(){
   finalLevelMusic = loadSound("assets/Music/FINALBOSSPOWERUP.wav");
   gameMusic  = loadSound("assets/Music/gameMusic.mp3");
   bounce = loadSound("assets/Music/bounceSoundEffect.wav");
+  winScreenMusic = loadSound("assets/Music/congratulations.mp3");
 }
 
-// This sets up the canvas, the state of the screen
+// This sets up the canvas, the state of the screen and some other small things
 function setup() {
   gameMusic.amp(0.2);
+  gameMusic.loop();
   finalLevelMusic.amp(0.0);
   finalLevelMusic.loop();
-  gameMusic.loop();
+  winScreenMusic.amp(0,0);
+  winScreenMusic.loop();
   let canvas = createCanvas(800, 800);
   canvas.position(windowWidth/4, 0);
   noCursor();
-  state = 1;
+  score = 0;
+  visualScore = 0;
 }
 
-// Displays all the aspects of the game.
+// Displays all the aspects of the game
 function draw() {
   gameScreens();
 }
@@ -69,6 +73,13 @@ function draw() {
 function platformPlacement() {
   limits = constrain(mouseX - 46, 0, 708);
   image(platform, limits, 700);
+}
+
+// This function makes the ball
+function displayBall(){
+  ballSize = 20;
+  fill(255);
+  ellipse(x, y, ballSize, ballSize);
 }
 
 // This function moves the ball around the screen
@@ -85,7 +96,6 @@ function moveTheBall(){
     dy = -dy;
     bounce.play();
   }
-
   if (y === 700 && x < limits + platform.width && x > limits+ 88 - platform.width/2) {
     dy = -dy;
     dx = Math.abs(dx);
@@ -93,7 +103,7 @@ function moveTheBall(){
   }
   if (y === 700 && x < limits + platform.width/2 && x > limits+ 88 - platform.width) {
     dy = -dy;
-    dx = -(Math.abs(dx));
+    dx = -Math.abs(dx);
     bounce.play();
   }
   if (y === 700 && x === platform.width/2) {
@@ -101,30 +111,12 @@ function moveTheBall(){
     dx = random(dx,-dx);
     bounce.play();
   }
-
   if (y >= 775){
-    state = 5;
+    state = 6;
   }
 }
 
-// This function makes the ball
-function displayBall(){
-  ballSize = 20;
-  fill(255);
-  ellipse(x, y, ballSize, ballSize);
-}
-
-// This function restarts the game
-function restart(){
-  x = 250;
-  y = 400;
-  score = 0;
-  brickStatusState = true;
-  finalLevelMusic.amp(0.0);
-  gameMusic.amp(0.2);
-}
-
-// This gives all the values in the array a status of 1
+// This gives all the values in the array a status
 function brickStatus() {
   if (brickStatusState === true && state === 2){
     for(let c=0; c<brickColumnCount; c++) {
@@ -155,96 +147,20 @@ function brickStatus() {
   }
 }
 
-
-// This function displays all the different game screens like the intro screen the levels and the end screen
-function gameScreens() {
-  if (state === 1){
-    background(introScreen);
-    textAlign(CENTER);
-    fill(150, 241, 247);
-    textSize(150);
-    textStyle(BOLD);
-    textFont("Agency FB");
-    text("B R E A K E R", 400,250);
-
-    textSize(32);
-    text("P r e s s   S P A C E   o r  C L I C K  t o   p l a y", 400, 550);
-
-    if (mouseIsPressed){
-      state = 2;
-    }
-    if (keyIsPressed){
-      if (keyCode === 32) {
-        state = 2;
-      }
-    }
-  }
-
-  if (state === 2) {
-    level = 1;
-    background(introScreen);
-    brickStatus();
-    platformPlacement();
-    displayBall();
-    moveTheBall();
-    brickSpawn();
-    colissionDectection();
-
-  }
-
-  if (state === 3) {
-    level = 3;
-    background(introScreen);
-    brickStatus();
-    platformPlacement();
-    displayBall();
-    moveTheBall();
-    brickSpawn();
-    colissionDectection();
-
-  }
-  if (state === 4){
-    level = 5;
-    background(finalLevel);
-    brickStatus();
-    platformPlacement();
-    displayBall();
-    moveTheBall();
-    brickSpawn();
-    colissionDectection();
-  }
-
-  if (state === 5){
-    background(introScreen);
-    textAlign(CENTER);
-    fill(150, 241, 247);
-    textSize(130);
-    textStyle(BOLD);
-    textFont("Agency FB");
-    text("G A M E  O V E R", 400,250);
-
-    textSize(32);
-    text("C L I C K  t o  R E S T A R T", 400, 550);
-    if (mouseIsPressed){
-      state = 1;
-      restart();
-    }
-  }
-}
-
 // This function check whether or not the ball hits a brick
 function colissionDectection() {
   for (let c=0; c<brickColumnCount; c++) {
     for (let r=0; r<brickRowCount; r++) {
       let b = bricks[c][r];
-      if(b.status != 0) {
+      if(b.status !== 0) {
         if (x >= b.x && x <= b.x + 72 && y >= b.y && y <= b.y + 32){
           score++;
+          visualScore = visualScore + 10;
           dy = -dy;
           b.status = b.status - 1;
           bounce.play();
         }
-        if( state != 3 && score === brickRowCount*brickColumnCount*state) {
+        if( state !== 3 && score === brickRowCount*brickColumnCount*state) {
           restart();
           state++;
         }
@@ -257,6 +173,16 @@ function colissionDectection() {
       }
     }
   }
+}
+
+// This function displays the "fake" score on the screen
+function displayScore() {
+  textAlign(LEFT);
+  fill(150, 241, 247);
+  textSize(40);
+  textStyle(BOLD);
+  textFont("Agency FB");
+  text("SCORE: " + visualScore, 13, 785);
 }
 
 //This function places all the bricks on the canvas
@@ -356,4 +282,105 @@ function brickSpawn() {
       }
     }
   }
+}
+
+// This function displays all the different game screens like the intro screen the levels and the end screen
+function gameScreens() {
+  if (state === 1){
+    background(introScreen);
+    textAlign(CENTER);
+    fill(150, 241, 247);
+    textSize(150);
+    textStyle(BOLD);
+    textFont("Agency FB");
+    text("B R E A K E R", 400,250);
+    textSize(32);
+    text("P r e s s   S P A C E   o r  C L I C K  t o   p l a y", 400, 550);
+    if (mouseIsPressed){
+      state = 2;
+    }
+    if (keyIsPressed){
+      if (keyCode === 32) {
+        state = 2;
+      }
+    }
+  }
+  if (state === 2) {
+    level = 1;
+    background(introScreen);
+    brickStatus();
+    platformPlacement();
+    displayBall();
+    moveTheBall();
+    brickSpawn();
+    colissionDectection();
+    displayScore();
+  }
+  if (state === 3) {
+    level = 3;
+    background(introScreen);
+    brickStatus();
+    platformPlacement();
+    displayBall();
+    moveTheBall();
+    brickSpawn();
+    colissionDectection();
+    displayScore();
+  }
+  if (state === 4){
+    level = 5;
+    gameMusic.stop();
+    finalLevelMusic.amp(1);
+    background(finalLevel);
+    brickStatus();
+    platformPlacement();
+    displayBall();
+    moveTheBall();
+    brickSpawn();
+    colissionDectection();
+    displayScore();
+  }
+  // This is the end screen
+  if (state === 5) {
+    finalLevelMusic.stop();
+    winScreenMusic.amp(1);
+    background(introScreen);
+    textAlign(CENTER);
+    fill(150, 241, 247);
+    textSize(80);
+    textStyle(BOLD);
+    textFont("Agency FB");
+    text("C O N G R A D U L A T I O N S", 400,height/2-50);
+    text("Y O U   W I N", 400,height/2+100);
+  }
+  // This is the game over screen
+  if (state === 6){
+    background(introScreen);
+    textAlign(CENTER);
+    fill(150, 241, 247);
+    textSize(130);
+    textStyle(BOLD);
+    textFont("Agency FB");
+    text("G A M E  O V E R", 400,250);
+    textSize(32);
+    text("C L I C K  t o  R E S T A R T", 400, 550);
+    if (mouseIsPressed){
+      state = 1;
+      displayScore();
+      restart();
+    }
+  }
+}
+
+// This function restarts the game
+function restart(){
+  gameMusic.stop();
+  finalLevelMusic.stop();
+  gameMusic.loop();
+  gameMusic.amp(1);
+  x = 250;
+  y = 400;
+  score = 0;
+  visualScore = 0;
+  brickStatusState = true;
 }
